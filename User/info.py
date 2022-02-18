38d19ed4e9dd7309e19192d731f4a181e730fb71
hitesh <hiteshtare3@gmail.com>
2022-02-18 23:09:49 +0530

    intilize commit

---
+++ User/info.py
@@ -0,0 +1,7 @@
+import smtplib
+
+EMAIL_USE_TLS= True
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_HOST_USER='django.demo.login@gmail.com'
+EMAIL_HOST_PASSWORD='django@12'
+EMAIL_PORT=587
---
+++ authUser/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+# from library import views 
+from . import views
+
+urlpatterns = [
+   path('',views.home, name="home"),
+   path('signup',views.signup, name="signup"),
+   path('signin',views.signin, name="signin"),
+   path('signout',views.signout, name="signout"),
+    path('activate/<uidb64>/<token>',views.activate, name="activate"),
+
+
+]
+
